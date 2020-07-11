4f7c7579bde49a2b399c820b1b70f8cb01557caf
André Luiz <alm28062001@gmail.com>
2020-07-11 20:20:43 -0300

    integração mongoose

--- src/http-common.js
+++ src/http-common.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
+  // endereço da api
   baseURL: 'http://localhost:8081/',
   headers: {
     'Content-type': 'application/json',
