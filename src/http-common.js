e792b398dcbb59e31e3c85caab15b6a8d93f0719
André Luiz <alm28062001@gmail.com>
2020-07-12 00:12:57 -0300

    ahhhhh

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 export default axios.create({
   // endereço da api
-  baseURL: 'https://grade-back-api.herokuapp.com',
+  baseURL: 'https://grade-back-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
